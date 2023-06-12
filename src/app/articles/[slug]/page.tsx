15287ab55f64ddab3cf6c6611131a138d73b6445
koralle <mkhd7930@gmail.com>
2023-06-12 23:42:09 +0900

    wip: Newt + Next.js (App Router)に入門した

---
+++ src/app/articles/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import { getArticleBySlug, getArticles } from "@/lib/newt";
+
+const Article = async ({ params }) => {
+  const { slug } = params;
+  const article = await getArticleBySlug(slug);
+
+  if (!article) return;
+
+  return (
+    <>
+      <main>
+        <h1>{article?.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: article.body }}></div>
+      </main>
+    </>
+  );
+};
+
+export default Article;
+
+export const generateStaticParams = async () => {
+  const articles = await getArticles();
+
+  return articles.map((article) => {
+    article.slug;
+  });
+};
---
+++ src/types/article.ts
@@ -0,0 +1,8 @@
+type Article = {
+  _id: string;
+  title: string;
+  slug: string;
+  body: string;
+};
+
+export type { Article };
