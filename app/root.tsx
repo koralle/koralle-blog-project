ac8aff63c521fd08213af225c70938cef11b14e2
koralle <mkhd7930@gmail.com>
2023-09-11 01:39:45 +0900

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,8 @@ import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
+import { Header } from "./routes/header";
+
 export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [])];
 
 export default function App() {
---
+++ app/routes/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <span>あざらしになりたい</span>
+
+      <span>About</span>
+      <span>Articles</span>
+    </header>
+  )
+}
+export { Header }
